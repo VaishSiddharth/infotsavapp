e3f36120a2a4f1970a2c65afb922398cb9be1eb4
Siddharth Vaish <sidvaish97@gmail.com>
2018-12-22 16:48:07 +0530

    changes in splash screen

--- app/src/main/java/com/infotsav/test/WelcomeActivity.java
+++ app/src/main/java/com/infotsav/test/WelcomeActivity.java
@@ -6,32 +6,35 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 import com.infotsav.test.MainMenuUtils.MainActivity;
 
 public class WelcomeActivity extends AppCompatActivity {
-    private final int SPLASH_DISPLAY_LENGTH = 1000;
-    LinearLayout line1,line3;
+    private final int SPLASH_DISPLAY_LENGTH = 10000;
+    LinearLayout line3;
+    ImageView line1;
     Animation uptodown,downtoup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        line1 =findViewById(R.id.line1);
+        line3 =findViewById(R.id.line3);
+        uptodown = AnimationUtils.loadAnimation(this,R.anim.uptodown);
+        downtoup = AnimationUtils.loadAnimation(this,R.anim.downtoup);
+        line1.setAnimation(uptodown);
+        line3.setAnimation(downtoup);
         new Handler().postDelayed(new Runnable(){
             @Override
             public void run() {
-                /* Create an Intent that will start the MainActivity. */
+                // Create an Intent that will start the MainActivity.
                 Intent mainIntent = new Intent(WelcomeActivity.this,MainActivity.class);
                 WelcomeActivity.this.startActivity(mainIntent);
                 WelcomeActivity.this.finish();
             }
         }, SPLASH_DISPLAY_LENGTH);
-        line1 = (LinearLayout) findViewById(R.id.line1);
-        line3 = (LinearLayout) findViewById(R.id.line3);
-        uptodown = AnimationUtils.loadAnimation(this,R.anim.uptodown);
-        downtoup = AnimationUtils.loadAnimation(this,R.anim.downtoup);
-        line1.setAnimation(uptodown);
-        line3.setAnimation(downtoup);
+
     }
 }
