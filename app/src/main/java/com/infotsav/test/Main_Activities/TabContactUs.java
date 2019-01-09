f425776c18a72e0a7c7cad7347d1bd503b3f1cce
Siddharth Vaish <sidvaish97@gmail.com>
2019-01-10 03:18:08 +0530

    changes in tabcontactus

--- app/src/main/java/com/infotsav/test/Main_Activities/TabContactUs.java
+++ app/src/main/java/com/infotsav/test/Main_Activities/TabContactUs.java
@@ -137,36 +137,36 @@ public class TabContactUs extends AppCompatActivity {
             mContactus_detailslist =new ArrayList<>();
             if( getArguments().getInt(ARG_SECTION_NUMBER)==1)
             {
-                mContactus_detailslist.add(new Contactus_details("1","JUHI TIWARI","juhi@infotsav.in","Coordinator","9461155105","https://firebasestorage.googleapis.com/v0/b/infotsavapp19.appspot.com/o/imageedit_28_5023319710.gif?alt=media&token=dc7cf248-b04a-46fb-a59c-908347126cf8"));
-                mContactus_detailslist.add(new Contactus_details("2","MUKUL SIKKA","mukul@infotsav.in","Coordinator","7470485414","https://firebasestorage.googleapis.com/v0/b/infotsavapp19.appspot.com/o/imageedit_26_7675552517.gif?alt=media&token=d12244bc-dce6-41dd-ab0f-990d4a513752"));
-                mContactus_detailslist.add(new Contactus_details("3","PRATYUSH RANJAN","pratyush@infotsav.in","Coordinator","9598319698","https://firebasestorage.googleapis.com/v0/b/infotsavapp19.appspot.com/o/imageedit_18_7239106357.gif?alt=media&token=93e95a6e-1f92-47d1-a245-5c54c9a7b5f7"));
-                mContactus_detailslist.add(new Contactus_details("4","SHUBHANKER SRIVASTAVA","shubhanker@infotsav.in","Coordinator","9454932137","https://firebasestorage.googleapis.com/v0/b/infotsavapp19.appspot.com/o/imageedit_55_9286984931.gif?alt=media&token=51dbaae1-e966-465e-bd81-1801380200ed"));
-                mContactus_detailslist.add(new Contactus_details("20","ASIF MOHAMMED","asif@infotsav.in","Technical Head","9848994293","https://firebasestorage.googleapis.com/v0/b/infotsavapp19.appspot.com/o/imageedit_52_2855844639.gif?alt=media&token=5e12264a-f8ac-46a1-b2a0-1a89840af154"));
-                mContactus_detailslist.add(new Contactus_details("21","NEHA SHARMA","neha@infotsav.in","Managerial Head","7722647747","https://firebasestorage.googleapis.com/v0/b/infotsavapp19.appspot.com/o/imageedit_59_3303772453.gif?alt=media&token=7be4f31e-929b-4627-beb3-cacec9dcf704"));
-                mContactus_detailslist.add(new Contactus_details("5","ANKIT BANSAL","ankit@infotsav.in","EMT","9810705130","https://firebasestorage.googleapis.com/v0/b/infotsavapp19.appspot.com/o/imageedit_43_8535333223.gif?alt=media&token=6729262f-90ca-4f02-be52-81bbf12b920b"));
-                mContactus_detailslist.add(new Contactus_details("6","AYUSHI RASTOGI","ayushi@infotsav.in","EMT","755519490","https://firebasestorage.googleapis.com/v0/b/infotsavapp19.appspot.com/o/imageedit_41_2055629182.gif?alt=media&token=e6a158d6-067f-4a84-b143-5e65a20ad4a8"));
-                mContactus_detailslist.add(new Contactus_details("7","DEEPANKAR SINGH","deepankar@infotsav.in","EMT","744053579","https://firebasestorage.googleapis.com/v0/b/infotsavapp19.appspot.com/o/imageedit_32_7604271821.gif?alt=media&token=fc4dab04-eb88-40af-8274-b4368270c220"));
-                mContactus_detailslist.add(new Contactus_details("8","JUHI PURSWANI","juhi@infotsav.in","EMT","7823666989","https://firebasestorage.googleapis.com/v0/b/infotsavapp19.appspot.com/o/imageedit_16_6628213714.gif?alt=media&token=6bee03a6-ff29-4727-a721-e95aa3f1973d"));
-                mContactus_detailslist.add(new Contactus_details("9","PRAKHAR BANSAL","prakhar@infotsav.in","EMT","7351480777","https://firebasestorage.googleapis.com/v0/b/infotsavapp19.appspot.com/o/imageedit_39_3114977136.gif?alt=media&token=9febd456-517e-4fa7-bcbf-3ff3aecbf2fe"));
-                mContactus_detailslist.add(new Contactus_details("10","SHUBHAM BHATTACHARYYA","shubham@infotsav.in","EMT","7507008911","https://firebasestorage.googleapis.com/v0/b/infotsavapp19.appspot.com/o/imageedit_50_6400638181.gif?alt=media&token=da59f3c8-a8ef-4d19-9d3d-796e10439bd2"));
-                mContactus_detailslist.add(new Contactus_details("11","SWARUP PADHY","swarup@infotsav.in","EMT","9873626099","https://firebasestorage.googleapis.com/v0/b/infotsavapp19.appspot.com/o/imageedit_12_2909797217.gif?alt=media&token=8f363d64-deb8-401f-acbf-1fff0e0bb5f4"));
-                mContactus_detailslist.add(new Contactus_details("12","VIVEK ALLADI","vivek@infotsav.in","EMT","8885260958","https://firebasestorage.googleapis.com/v0/b/infotsavapp19.appspot.com/o/imageedit_1_9496876329.gif?alt=media&token=8187804c-aec3-4a86-97de-fbd869026142"));
-                mContactus_detailslist.add(new Contactus_details("13","DINESH VERRA","dinesh@infotsav.in","EMT","8179515709","https://firebasestorage.googleapis.com/v0/b/infotsavapp19.appspot.com/o/imageedit_30_2090489510.gif?alt=media&token=fa7a9951-6957-4b06-96ec-eee5cdab8a34"));
-                mContactus_detailslist.add(new Contactus_details("14","NILAY GUPTA","nilay@infotsav.in","EMT","9044712480","https://firebasestorage.googleapis.com/v0/b/infotsavapp19.appspot.com/o/imageedit_24_3460571397.gif?alt=media&token=db384c6a-6283-4766-a806-0580a0d13b2d"));
-                mContactus_detailslist.add(new Contactus_details("15","PALAK JAIN","palak@infotsav.in","EMT","7943486494","https://firebasestorage.googleapis.com/v0/b/infotsavapp19.appspot.com/o/imageedit_22_6417364399.gif?alt=media&token=bbcb1541-fe69-4c11-97f4-0be281d199a8"));
-                mContactus_detailslist.add(new Contactus_details("16","POORVA KHANDARE","poorva@infotsav.in","EMT","7859535921","https://firebasestorage.googleapis.com/v0/b/infotsavapp19.appspot.com/o/imageedit_20_7603991370.gif?alt=media&token=49d21dcd-9db1-4198-85f8-c97aa617e3a0"));
-                mContactus_detailslist.add(new Contactus_details("17","PRIYANSH GAHARANA","priyansh@infotsav.in","EMT","9452685369","https://firebasestorage.googleapis.com/v0/b/infotsavapp19.appspot.com/o/imageedit_45_4131785817.gif?alt=media&token=d34e6555-ac3d-49f8-95aa-685bea3d06a4"));
-                mContactus_detailslist.add(new Contactus_details("18","TANMAY SHARMA","tanmay@infotsav.in","EMT","9549052975","https://firebasestorage.googleapis.com/v0/b/infotsavapp19.appspot.com/o/vmal.jpg?alt=media&token=34eb3c9c-dc3c-441d-b524-15cf657635c8"));
-                mContactus_detailslist.add(new Contactus_details("19","VISHESH KHANDELWAL","vishesh@infotsav.in","EMT","7678122119","https://firebasestorage.googleapis.com/v0/b/infotsavapp19.appspot.com/o/imageedit_3_9372509889.gif?alt=media&token=488e50ef-7b66-44fe-8e6a-9c8b43e89404"));
+                mContactus_detailslist.add(new Contactus_details("1","JUHI TIWARI","juhi@infotsav.in","Coordinator","9461155105",R.drawable.jjuhi));
+                mContactus_detailslist.add(new Contactus_details("2","MUKUL SIKKA","mukul@infotsav.in","Coordinator","7470485414",R.drawable.sikka));
+                mContactus_detailslist.add(new Contactus_details("3","PRATYUSH RANJAN","pratyush@infotsav.in","Coordinator","9598319698",R.drawable.pratyush));
+                mContactus_detailslist.add(new Contactus_details("4","SHUBHANKER SRIVASTAVA","shubhanker@infotsav.in","Coordinator","9454932137",R.drawable.shubhanker));
+                mContactus_detailslist.add(new Contactus_details("20","ASIF MOHAMMED","asif@infotsav.in","Technical Head","9848994293",R.drawable.asif));
+                mContactus_detailslist.add(new Contactus_details("21","NEHA SHARMA","neha@infotsav.in","Managerial Head","7722647747",R.drawable.neha));
+                mContactus_detailslist.add(new Contactus_details("5","ANKIT BANSAL","ankit@infotsav.in","EMT","9810705130",R.drawable.ankit));
+                mContactus_detailslist.add(new Contactus_details("6","AYUSHI RASTOGI","ayushi@infotsav.in","EMT","755519490",R.drawable.aayushi));
+                mContactus_detailslist.add(new Contactus_details("7","DEEPANKAR SINGH","deepankar@infotsav.in","EMT","744053579",R.drawable.deep));
+                mContactus_detailslist.add(new Contactus_details("8","JUHI PURSWANI","juhi@infotsav.in","EMT","7823666989",R.drawable.purush));
+                mContactus_detailslist.add(new Contactus_details("9","PRAKHAR BANSAL","prakhar@infotsav.in","EMT","7351480777",R.drawable.prakhar));
+                mContactus_detailslist.add(new Contactus_details("10","SHUBHAM BHATTACHARYYA","shubham@infotsav.in","EMT","7507008911",R.drawable.tutu));
+                mContactus_detailslist.add(new Contactus_details("11","SWARUP PADHY","swarup@infotsav.in","EMT","9873626099",R.drawable.swaroop));
+                mContactus_detailslist.add(new Contactus_details("12","VIVEK ALLADI","vivek@infotsav.in","EMT","8885260958",R.drawable.vivek));
+                mContactus_detailslist.add(new Contactus_details("13","DINESH VERRA","dinesh@infotsav.in","EMT","8179515709",R.drawable.verra));
+                mContactus_detailslist.add(new Contactus_details("14","NILAY GUPTA","nilay@infotsav.in","EMT","9044712480",R.drawable.nilay));
+                mContactus_detailslist.add(new Contactus_details("15","PALAK JAIN","palak@infotsav.in","EMT","7943486494",R.drawable.palak));
+                mContactus_detailslist.add(new Contactus_details("16","POORVA KHANDARE","poorva@infotsav.in","EMT","7859535921",R.drawable.poorva));
+                mContactus_detailslist.add(new Contactus_details("17","PRIYANSH GAHARANA","priyansh@infotsav.in","EMT","9452685369",R.drawable.alpha));
+                mContactus_detailslist.add(new Contactus_details("18","TANMAY SHARMA","tanmay@infotsav.in","EMT","9549052975",R.drawable.tanmay));
+                mContactus_detailslist.add(new Contactus_details("19","VISHESH KHANDELWAL","vishesh@infotsav.in","EMT","7678122119",R.drawable.vishesh));
             }
             if( getArguments().getInt(ARG_SECTION_NUMBER)==2) {
-                mContactus_detailslist.add(new Contactus_details("5", "AVIHARSH SHUKLA", "sidvaish4@gmail.com", "Head App Developer", "8765114937", "https://firebasestorage.googleapis.com/v0/b/infotsavapp19.appspot.com/o/imageedit_57_5881310723.gif?alt=media&token=e679bb0b-08aa-4a7f-8da7-9ee8eba7cbf3"));
-                mContactus_detailslist.add(new Contactus_details("6", "SIDDHARTH VAISH", "sidvaish97@gmail.com", "Head App Developer", "9149386335", "https://firebasestorage.googleapis.com/v0/b/infotsavapp19.appspot.com/o/imageedit_7_7898158568.gif?alt=media&token=da5bb680-411e-43ed-8627-104cdf9991c3"));
-                mContactus_detailslist.add(new Contactus_details("7", "HIMADRI SINGH", "himsingh1612@gmail.com", "App Developer", "8449569368", "https://firebasestorage.googleapis.com/v0/b/infotsavapp19.appspot.com/o/imageedit_61_9407681319.gif?alt=media&token=b6d18d34-3ec1-41e0-9481-b6321bc64279"));
-                mContactus_detailslist.add(new Contactus_details("11", "SHUBHAM BHAWSAR", "bhawsarshubham741@gmail.com", "App Developer", "8878674076", "https://firebasestorage.googleapis.com/v0/b/infotsavapp19.appspot.com/o/imageedit_14_2843577101.gif?alt=media&token=759c335a-3752-4c5e-8900-d705ad77b0a4"));
-                mContactus_detailslist.add(new Contactus_details("8", "DEEPAK PALIWAL", "deepakkpaliwal@gmail.com", "App Developer", "8769449558", "https://firebasestorage.googleapis.com/v0/b/infotsavapp19.appspot.com/o/imageedit_34_2360165048.gif?alt=media&token=ddd55b78-c500-4908-81d9-161cbbf5baeb"));
-                mContactus_detailslist.add(new Contactus_details("9", "ABHISHEK CHAURASIA", "abhi.chaurasia14@gmail.com", "App Developer", "9919098817", "https://firebasestorage.googleapis.com/v0/b/infotsavapp19.appspot.com/o/imageedit_37_7841767981.gif?alt=media&token=0fc84c06-f584-475a-81b7-2484398d3ddb"));
-                mContactus_detailslist.add(new Contactus_details("10", "ADITYA SHANKAR MISHRA", "adityashankarmishra3@gmail.com", "App Developer", "9260970643", "https://firebasestorage.googleapis.com/v0/b/infotsavapp19.appspot.com/o/imageedit_47_6166486762.gif?alt=media&token=6b520c9d-6dbd-4ec5-a3fd-7d6d8cde9e80"));
+                mContactus_detailslist.add(new Contactus_details("5", "AVIHARSH SHUKLA", "sidvaish4@gmail.com", "Head App Developer", "8765114937", R.drawable.aviharsh));
+                mContactus_detailslist.add(new Contactus_details("6", "SIDDHARTH VAISH", "sidvaish97@gmail.com", "Head App Developer", "9149386335", R.drawable.sid));
+                mContactus_detailslist.add(new Contactus_details("7", "HIMADRI SINGH", "himsingh1612@gmail.com", "App Developer", "8449569368", R.drawable.himadri));
+                mContactus_detailslist.add(new Contactus_details("11", "SHUBHAM BHAWSAR", "bhawsarshubham741@gmail.com", "App Developer", "8878674076", R.drawable.shubh));
+                mContactus_detailslist.add(new Contactus_details("8", "DEEPAK PALIWAL", "deepakkpaliwal@gmail.com", "App Developer", "8769449558", R.drawable.deepak));
+                mContactus_detailslist.add(new Contactus_details("9", "ABHISHEK CHAURASIA", "abhi.chaurasia14@gmail.com", "App Developer", "9919098817", R.drawable.abhishek));
+                mContactus_detailslist.add(new Contactus_details("10", "ADITYA SHANKAR MISHRA", "adityashankarmishra3@gmail.com", "App Developer", "9260970643", R.drawable.adi));
 
             }
             adapter= new ContactusAdapter( getActivity().getApplicationContext(), mContactus_detailslist);
