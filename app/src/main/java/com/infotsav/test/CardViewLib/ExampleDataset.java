c132dda61629cd13af83ecb874ff95d209918a5e
Siddharth Vaish <sidvaish97@gmail.com>
2019-01-13 15:05:49 +0530

    size dcreased by removing files from drawable

--- app/src/main/java/com/infotsav/test/CardViewLib/ExampleDataset.java
+++ app/src/main/java/com/infotsav/test/CardViewLib/ExampleDataset.java
@@ -54,7 +54,7 @@ public class ExampleDataset {
         item6.setHeadTitle("Events");
         item6.setPersonMessage("Captain America’s costume in Infinity War is based on two separate looks from the comics");
         item6.setPersonName(facts[index7]);
-        item6.setPersonPictureResource(R.drawable.marjorie_ellis);
+        item6.setPersonPictureResource(R.drawable.aaron_bradley);
         item6.setListItems(prepareCommentsArray());
         dataset.add(item6);
 
@@ -64,7 +64,7 @@ public class ExampleDataset {
         item5.setHeadTitle("Register");
         item5.setPersonMessage("Marvel may be developing that Black Widow movie after all");
         item5.setPersonName(facts[index6]);
-        item5.setPersonPictureResource(R.drawable.marjorie_ellis);
+        item5.setPersonPictureResource(R.drawable.aaron_bradley);
         item5.setListItems(prepareCommentsArray());
         dataset.add(item5);
 
@@ -74,7 +74,7 @@ public class ExampleDataset {
         item4.setHeadTitle("Updates");
         item4.setPersonMessage("Tony Stark is still nursing an injured left arm");
         item4.setPersonName(facts[index5]);
-        item4.setPersonPictureResource(R.drawable.mattew_jordan);
+        item4.setPersonPictureResource(R.drawable.aaron_bradley);
         item4.setListItems(prepareCommentsArray());
         dataset.add(item4);
 
@@ -84,7 +84,7 @@ public class ExampleDataset {
         item3.setHeadTitle("Treasure Hunt");
         item3.setPersonMessage("In the comic books, Thanos erases half of the life in the universe");
         item3.setPersonName(facts[index4]);
-        item3.setPersonPictureResource(R.drawable.ross_rodriguez);
+        item3.setPersonPictureResource(R.drawable.aaron_bradley);
         item3.setListItems(prepareCommentsArray());
         dataset.add(item3);
 
@@ -104,7 +104,7 @@ public class ExampleDataset {
         item1a.setHeadTitle("About Us");
         item1a.setPersonMessage("Cur adelphis studere?");
         item1a.setPersonName(facts[index2]);
-        item1a.setPersonPictureResource(R.drawable.wallace_sutton);
+        item1a.setPersonPictureResource(R.drawable.aaron_bradley);
         item1a.setListItems(prepareCommentsArray());
         dataset.add(item1a);
 
@@ -114,7 +114,7 @@ public class ExampleDataset {
         item1.setHeadTitle("Contact Us");
         item1.setPersonMessage("Cur adelphis studere?");
         item1.setPersonName(facts[index1]);
-        item1.setPersonPictureResource(R.drawable.wallace_sutton);
+        item1.setPersonPictureResource(R.drawable.aaron_bradley);
         item1.setListItems(prepareCommentsArray());
         dataset.add(item1);
 
@@ -134,17 +134,17 @@ public class ExampleDataset {
         List<Comment> comments = new ArrayList<>();
         comments.addAll(Arrays.asList(
                 new Comment(R.drawable.aaron_bradley, "Aaron Bradley", "When the sensor experiments for deep space, all mermaids accelerate mysterious, vital moons.", "jan 12, 2014"),
-                new Comment(R.drawable.barry_allen, "Barry Allen", "It is a cold powerdrain, sir.", "jun 1, 2015"),
-                new Comment(R.drawable.bella_holmes, "Bella Holmes", "Particle of a calm shield, control the alignment!", "sep 21, 1937"),
-                new Comment(R.drawable.caroline_shaw, "Caroline Shaw", "The human kahless quickly promises the phenomenan.", "may 23, 1967"),
-                new Comment(R.drawable.connor_graham, "Connor Graham", "Ionic cannon at the infinity room was the sensor of voyage, imitated to a dead pathway.", "sep 1, 1972"),
+                new Comment(R.drawable.aaron_bradley, "Barry Allen", "It is a cold powerdrain, sir.", "jun 1, 2015"),
+                new Comment(R.drawable.aaron_bradley, "Bella Holmes", "Particle of a calm shield, control the alignment!", "sep 21, 1937"),
+                new Comment(R.drawable.aaron_bradley, "Caroline Shaw", "The human kahless quickly promises the phenomenan.", "may 23, 1967"),
+                new Comment(R.drawable.aaron_bradley, "Connor Graham", "Ionic cannon at the infinity room was the sensor of voyage, imitated to a dead pathway.", "sep 1, 1972"),
                // new Comment(R.drawable.deann_hunt, "Deann Hunt", "Vital particles, to the port.", "aug 13, 1995"),
-                new Comment(R.drawable.ella_cole, "Ella Cole", "Stars fly with hypnosis at the boldly infinity room!", "nov 18, 1952"),
-                new Comment(R.drawable.jayden_shaw, "Jayden Shaw", "Hypnosis, definition, and powerdrain.", "apr 1, 2013"),
-                new Comment(R.drawable.jerry_carrol, "Jerry Carrol", "When the queen experiments for nowhere, all particles control reliable, cold captains.", "nov 14, 1964"),
-                new Comment(R.drawable.lena_lucas, "Lena Lukas", "When the c-beam experiments for astral city, all cosmonauts acquire remarkable, virtual lieutenant commanders.", "may 4, 1965"),
-                new Comment(R.drawable.leonrd_kim, "Leonard Kim", "Starships walk with love at the cold parallel universe!", "jul 3, 1974"),
-                new Comment(R.drawable.marc_baker, "Mark Baker", "Friendship at the bridge that is when quirky green people yell.", "dec 24, 1989")));
+                new Comment(R.drawable.aaron_bradley, "Ella Cole", "Stars fly with hypnosis at the boldly infinity room!", "nov 18, 1952"),
+                new Comment(R.drawable.aaron_bradley, "Jayden Shaw", "Hypnosis, definition, and powerdrain.", "apr 1, 2013"),
+                new Comment(R.drawable.aaron_bradley, "Jerry Carrol", "When the queen experiments for nowhere, all particles control reliable, cold captains.", "nov 14, 1964"),
+                new Comment(R.drawable.aaron_bradley, "Lena Lukas", "When the c-beam experiments for astral city, all cosmonauts acquire remarkable, virtual lieutenant commanders.", "may 4, 1965"),
+                new Comment(R.drawable.aaron_bradley, "Leonard Kim", "Starships walk with love at the cold parallel universe!", "jul 3, 1974"),
+                new Comment(R.drawable.aaron_bradley, "Mark Baker", "Friendship at the bridge that is when quirky green people yell.", "dec 24, 1989")));
         Collections.shuffle(comments);
         return comments.subList(0, 6 + random.nextInt(5));
     }
